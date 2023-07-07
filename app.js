153e241faa3a28a4217d5a754634940ac534cb17
Gwenaël Magnenat <g.magnenat@gmail.com>
2023-07-07 11:12:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const http = require('http');
 
-http.createServer(function (req, res) {
-res.write("On the way to being a full snack engineer!");
-res.end();
+http
+	.createServer(function (req, res) {
+		res.write('Hey Richard, What is up?');
+		res.end();
+	})
+	.listen(3000);
 
-
-}
-).listen(3000);
-
-console.log("Server started on port 3000");
+console.log('Server started on port 3000');
