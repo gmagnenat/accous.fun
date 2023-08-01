3a0ecc4992df9fbaa98b8165985f9ce06b80d577
Gwenaël Magnenat <g.magnenat@gmail.com>
2023-08-01 13:29:04 +0200

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -7,9 +7,18 @@ app.get('/', function (req, res) {
 });
 
 server.on('request', app);
-
 server.listen(3000, function () {
 	console.log('server is running on port 3000');
+});
+
+// process listener
+process.on('SIGINT', () => {
+	wss.clients.forEach(function each(client) {
+		client.close();
+	});
+	server.close(() => {
+		shutdownDB();
+	});
 });
 
 /** Begin websocket */
@@ -27,6 +36,10 @@ wss.on('connection', function connection(ws) {
 		ws.send('Welcome to my server');
 	}
 
+	db.run(`INSERT INTO visitors (count, time)
+		VALUES (${numClients}, datetime('now'))
+	`);
+
 	ws.on('close', function close() {
 		console.log('A client has disconnected');
 	});
@@ -37,3 +50,29 @@ wss.broadcast = function broadcast(data) {
 		client.send(data);
 	});
 };
+
+/** End websockets */
+/** Begin database */
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+	db.run(`
+		CREATE TABLE visitors (
+			count INTEGER,
+			time TEXT
+		)
+	`);
+});
+
+function getCounts() {
+	db.each('SELECT * FROM visitors', (err, row) => {
+		console.log(row);
+	});
+}
+
+function shutdownDB() {
+	getCounts();
+	console.log('Shutting down db');
+	db.close();
+}
